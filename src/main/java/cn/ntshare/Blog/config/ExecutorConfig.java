e117ea747879fecb0b8e0807ff7ff0ddb037442e
3795 <seven.iwi@gmail.com>
2019-08-08 16:57:57 +0800

    更改线程池的配置

--- src/main/java/cn/ntshare/Blog/config/ExecutorConfig.java
+++ src/main/java/cn/ntshare/Blog/config/ExecutorConfig.java
@@ -21,18 +21,17 @@ public class ExecutorConfig {
     public Executor asyncServiceExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         // 配置核心线程数
-        executor.setCorePoolSize(5);
+        executor.setCorePoolSize(2);
         // 配置最大线程数
-        executor.setMaxPoolSize(10);
+        executor.setMaxPoolSize(5);
         // 配置队列大小
-        executor.setQueueCapacity(1000);
+        executor.setQueueCapacity(10);
 
         // 配置线程池中的线程的名称前缀
         executor.setThreadNamePrefix("async-service-");
 
-        // rejection-policy：当pool已经达到max size的时候，如何处理新任务
-        // CALLER_RUNS：不在新线程中执行任务，而是有调用者所在的线程来执行
-        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        // 线程池饱和时，直接拒绝新的任务，抛出异常
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.AbortPolicy());
 
         // 初始化线程
         executor.initialize();
